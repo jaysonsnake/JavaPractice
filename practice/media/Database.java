280605914b8ab869d0a0a635c92709f8966e8487
like4501@163.com <like4501@163.com>
2020-02-02 10:18:09 +0800

    finished 多态

--- practice/media/Database.java
+++ practice/media/Database.java
@@ -9,13 +9,25 @@ import java.util.List;
  */
 public class Database {
 
+    private List<Item> itemList = new ArrayList<>();
+
+    public void add(Item item) {
+        itemList.add(item);
+    }
+
+    public void list() {
+        for (Item item : itemList) {
+            item.print();
+        }
+    }
+
     public static void main(String[] args) {
         Database database = new Database();
-        Item itemCD = new CD("aaa", "aaa", 10, 60, "...");
-        Item itemDVD = new DVD("bbb", "bbb", 10, "...");
+        database.add(new CD("aaa", "aaa", 10, 60, "..."));
+        database.add(new CD("bbb", "bbb", 10, 60, "..."));
+        database.add(new DVD("ccc", "ccc", 10, "..."));
 
-        itemCD.print();
-        itemDVD.print();
+        database.list();
     }
 
 
